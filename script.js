a1495f24cd30dc4c58f049c5a5bb12adfad74fa9
Josh <joshuakibwage23@gmail.com>
2025-03-27 07:29:10 +0300

    Last commit

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-    
+   $("button").on("click", function(){
+    const randomNumber = Math.ceil(Math.random()* 88)
+    $.get(`https://rawcdn.githack.com/akabab/starwars-api/0.2.1/api/id/${randomNumber}.json`, 
+        function(data){
+            
+            $("h2.name").text(data['name'])
+            $("#character-image").attr("src", data['image'])
+            $("p.gender").text(data['gender'])
+
+        })
+   }) 
 })
